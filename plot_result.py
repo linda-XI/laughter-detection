057e385ad5b67065f24697085d8807f5cbdb77af
Yuxi Dai <s1974435@uhtred.inf.ed.ac.uk>
2023-03-08 14:44:05 +0000

    baseline model evaluate on fixed dataset

--- plot_result.py
+++ plot_result.py
@@ -1,3 +1,4 @@
 from visualise import plot_conf_matrix
+from visualise import plot_conf_matrix2
 
-plot_conf_matrix('eval/eval_base_on_fix','test',thresholds=[0.2,0.4,0.6,0.8],min_len=0.2, name = 'baselineonfix__conf_matrix_test')
+plot_conf_matrix2('eval/eval_base_on_fix','dev',thresholds=[0.2,0.4,0.6,0.8],min_len=0.2, name = 'baselineonfix__conf_matrix_dev')
