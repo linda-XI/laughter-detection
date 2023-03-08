dc8c95fecbc02da4e020de0400290e2510b44a00
linda-XI <55888555+linda-XI@users.noreply.github.com>
2023-03-08 17:53:38 +0000

    Update models.py

--- models.py
+++ models.py
@@ -324,7 +324,7 @@ class MobileNetV2(nn.Module):
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
 
-        input_channel = 1
+        input_channel = 64
         last_channel = 1280
 
         if inverted_residual_setting is None:
@@ -349,7 +349,7 @@ class MobileNetV2(nn.Module):
         input_channel = _make_divisible(input_channel * width_mult, round_nearest)
         self.last_channel = _make_divisible(last_channel * max(1.0, width_mult), round_nearest)
         features: List[nn.Module] = [
-            Conv2dNormActivation(3, input_channel, stride=2, norm_layer=norm_layer, activation_layer=nn.ReLU6)
+            Conv2dNormActivation(1, input_channel, stride=2, norm_layer=norm_layer, activation_layer=nn.ReLU6)
         ]
         # building inverted residual blocks
         for t, c, n, s in inverted_residual_setting:
