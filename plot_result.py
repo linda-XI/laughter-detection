bd0871f25d9e9fe4ce9632cea521c08671baf7c3
Yuxi Dai <s1974435@uhtred.inf.ed.ac.uk>
2023-03-08 13:34:31 +0000

    baseline test eval

--- plot_result.py
+++ plot_result.py
@@ -1,3 +1,3 @@
 from visualise import plot_conf_matrix
 
-plot_conf_matrix('data/eval_baseline','dev',thresholds=[0.2,0.4,0.6,0.8],min_len=0.2, name = 'baseline_conf_matrix', sub_dir = 'data/visual')
+plot_conf_matrix('eval','test',thresholds=[0.2,0.4,0.6,0.8],min_len=0.2, name = 'baseline_conf_matrix_test')
