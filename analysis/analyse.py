dc31c3fc3c9ead23cdff8732e747be52ae355863
Yuxi Dai <s1974435@uhtred.inf.ed.ac.uk>
2023-03-08 14:30:43 +0000

    baseline model evaluate on fixed dataset

--- analysis/analyse.py
+++ analysis/analyse.py
@@ -144,9 +144,10 @@ def laugh_match(pred_laugh, meeting_id, part_id):
     noise_mismatch = seg_index_overlap(prep.noise_index, pred_laugh, meeting_id, part_id)
     remain_mismatch = incorrect - speech_mismatch - silence_mismatch - noise_mismatch
 
-    assert remain_mismatch < 0.001, f"Accumulated false positives don't match the total incorrect time. Difference: {remain_mismatch}"
+    #assert remain_mismatch < 0.001, f"Accumulated false positives don't match the total incorrect time. Difference: {remain_mismatch}"
 
-    return(correct, incorrect, speech_mismatch, noise_mismatch, silence_mismatch)
+    #return(correct, incorrect, speech_mismatch, noise_mismatch, silence_mismatch)
+    return(correct, incorrect, speech_mismatch, noise_mismatch, silence_mismatch, remain_mismatch)
 
 
 def eval_preds(pred_per_meeting_df, meeting_id, threshold, min_len, print_stats=False):
@@ -158,7 +159,7 @@ def eval_preds(pred_per_meeting_df, meeting_id, threshold, min_len, print_stats=
     tot_fp_speech_time = 0
     tot_fp_noise_time = 0 
     tot_fp_silence_time = 0 
-
+    tot_fp_false_silence_time = 0
     tot_transc_laugh_time = prep.laugh_index[meeting_id]['tot_len']
     num_of_tranc_laughs = parse.laugh_only_df[parse.laugh_only_df['meeting_id']
                                               == meeting_id].shape[0]
@@ -189,12 +190,14 @@ def eval_preds(pred_per_meeting_df, meeting_id, threshold, min_len, print_stats=
                 # Append interval to total predicted frames for this participant
                 part_pred_frames = part_pred_frames | pred_laugh
 
-            corr, incorr, speech, noise, silence =  laugh_match(part_pred_frames, meeting_id, part_id)
+            #corr, incorr, speech, noise, silence =  laugh_match(part_pred_frames, meeting_id, part_id)
+            corr, incorr, speech, noise, silence, false_silence =  laugh_match(part_pred_frames, meeting_id, part_id)
             tot_corr_pred_time += corr
             tot_incorr_pred_time += incorr
             tot_fp_speech_time += speech
             tot_fp_noise_time += noise
             tot_fp_silence_time += silence
+            tot_fp_false_silence_time += false_silence
 
     tot_predicted_time = tot_corr_pred_time + tot_incorr_pred_time
     # If there is no predicted laughter time for this meeting -> precision=1
@@ -222,7 +225,10 @@ def eval_preds(pred_per_meeting_df, meeting_id, threshold, min_len, print_stats=
 
     return[meeting_id, threshold, min_len, prec, recall, tot_corr_pred_time, tot_predicted_time,
            tot_transc_laugh_time, num_of_pred_laughs, num_of_VALID_pred_laughs, num_of_tranc_laughs, 
-           tot_fp_speech_time, tot_fp_noise_time, tot_fp_silence_time]
+           tot_fp_speech_time, tot_fp_noise_time, tot_fp_silence_time, tot_fp_false_silence_time]
+    #return[meeting_id, threshold, min_len, prec, recall, tot_corr_pred_time, tot_predicted_time,
+     #                   tot_transc_laugh_time, num_of_pred_laughs, num_of_VALID_pred_laughs, num_of_tranc_laughs,
+      #                             tot_fp_speech_time, tot_fp_noise_time, tot_fp_silence_time]
 
 def create_evaluation_df(path, out_path, use_cache=False):
     """
@@ -252,9 +258,12 @@ def create_evaluation_df(path, out_path, use_cache=False):
                     all_evals.append(out)
                     # Log progress
 
+        #cols = ['meeting', 'threshold', 'min_len', 'precision', 'recall',
+               # 'corr_pred_time', 'tot_pred_time', 'tot_transc_laugh_time', 'num_of_pred_laughs', 'valid_pred_laughs', 'num_of_transc_laughs',
+               # 'tot_fp_speech_time', 'tot_fp_noise_time', 'tot_fp_silence_time']
         cols = ['meeting', 'threshold', 'min_len', 'precision', 'recall',
                 'corr_pred_time', 'tot_pred_time', 'tot_transc_laugh_time', 'num_of_pred_laughs', 'valid_pred_laughs', 'num_of_transc_laughs',
-                'tot_fp_speech_time', 'tot_fp_noise_time', 'tot_fp_silence_time']
+                'tot_fp_speech_time', 'tot_fp_noise_time', 'tot_fp_silence_time','tot_fp_false_silence_time']
         if len(cols) != len(all_evals[0]):
             raise Exception(
                 f'List returned by eval_preds() has wrong length. Expected length: {len(cols)}. Found: {len(all_evals[0])}.')
--- plot_result.py
+++ plot_result.py
@@ -1,3 +1,3 @@
 from visualise import plot_conf_matrix
 
-plot_conf_matrix('eval','test',thresholds=[0.2,0.4,0.6,0.8],min_len=0.2, name = 'baseline_conf_matrix_test')
+plot_conf_matrix('eval/eval_base_on_fix','test',thresholds=[0.2,0.4,0.6,0.8],min_len=0.2, name = 'baselineonfix__conf_matrix_test')
