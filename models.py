fa0666c80e74d2e26fabb7653493b7d2851aac56
linda-XI <55888555+linda-XI@users.noreply.github.com>
2023-03-12 17:47:24 +0000

    Update models.py

--- models.py
+++ models.py
@@ -684,6 +684,70 @@ class ResNetBigger_DW(nn.Module):
             b.to(device)
         self.to(device)
 
+class ResNetBigger_DW2(nn.Module):
+    def __init__(self, num_classes=1, dropout_rate=0.5, linear_layer_size=192, filter_sizes=[64, 32, 16, 16]):
+        super(ResNetBigger_DW2, self).__init__()
+        print(f"training with dropout={dropout_rate}")
+        # Initial input conv
+        self.conv1 = nn.Conv2d(
+            in_channels=1, out_channels=64, kernel_size=(3, 3),
+            stride=1, padding=1, bias=False
+        )
+
+        self.bn1 = nn.BatchNorm2d(64)
+
+        self.linear_layer_size = linear_layer_size
+
+        self.filter_sizes = filter_sizes
+
+        self.block1 = self._create_block(64, filter_sizes[0], stride=1)
+        self.block2 = self._create_block(
+            filter_sizes[0], filter_sizes[1], stride=2)
+        self.block3 = self._create_block(
+            filter_sizes[1], filter_sizes[2], stride=2)
+        self.block4 = self._create_block(
+            filter_sizes[2], filter_sizes[3], stride=2)
+        self.bn2 = nn.BatchNorm1d(linear_layer_size)
+        self.bn3 = nn.BatchNorm1d(32)
+        self.linear1 = nn.Linear(linear_layer_size, 32)
+        self.linear2 = nn.Linear(32, num_classes)
+
+        self.dropout = nn.Dropout(dropout_rate)
+
+        self.global_step = 0
+        self.epoch = 0
+        self.best_val_loss = np.inf
+
+    # A block is just two residual blocks for ResNet18
+    def _create_block(self, in_channels, out_channels, stride):
+        return nn.Sequential(
+            ResidualBlock_DW(in_channels, out_channels, stride),
+            ResidualBlock_DW(out_channels, out_channels, 1)
+        )
+
+    def forward(self, x):
+        # Output of one layer becomes input to the next
+        out = nn.ReLU()(self.bn1(self.conv1(x)))
+        out = self.block1(out)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.block4(out)
+        out = nn.AvgPool2d(4)(out)
+        out = out.view(out.size(0), -1)
+        out = self.bn2(out)
+        out = self.dropout(out)
+        out = self.linear1(out)
+        out = self.bn3(out)
+        out = self.dropout(out)
+        out = F.relu(out)
+        out = self.linear2(out)
+        return out
+
+    def set_device(self, device):
+        for b in [self.block1, self.block2, self.block3, self.block4]:
+            b.to(device)
+        self.to(device)
+
         
 
 class ResNetNoBN(nn.Module):
