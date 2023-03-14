f1433b32252bd624b112bee39aad1d539acf384c
Yuxi Dai <s1974435@uhtred.inf.ed.ac.uk>
2023-03-14 11:38:19 +0000

    change

--- plot_result.py
+++ plot_result.py
@@ -1,4 +1,7 @@
 from visualise import plot_conf_matrix
 from visualise import plot_conf_matrix2
+input_dir = 'eval/eval_dw'
+plot_conf_matrix(input_dir,'test',thresholds=[0.2,0.4,0.6,0.8],min_len=0.2, name = 'res_dw18_test')
+plot_conf_matrix(input_dir,'dev',thresholds=[0.2,0.4,0.6,0.8],min_len=0.2, name = 'res_dw18_dev')
 
-plot_conf_matrix('eval/eval_dw','test',thresholds=[0.2,0.4,0.6,0.8],min_len=0.2, name = 'res_dw18_test')
+
