a3b14b3280647a5170ba65ce7837c9d914fbf99c
Yuxi Dai <s1974435@uhtred.inf.ed.ac.uk>
2023-11-02 14:56:36 +0000

    add dw plot

--- plot_result.py
+++ plot_result.py
@@ -1,7 +1,7 @@
 from visualise import plot_conf_matrix
 from visualise import plot_conf_matrix2
-input_dir = 'eval/eval_dw'
-plot_conf_matrix(input_dir,'test',thresholds=[0.2,0.4,0.6,0.8],min_len=0.2, name = 'res_dw18_test')
-plot_conf_matrix(input_dir,'dev',thresholds=[0.2,0.4,0.6,0.8],min_len=0.2, name = 'res_dw18_dev')
+input_dir = 'eval/320/dw'
+plot_conf_matrix(input_dir,'test',thresholds=[0.2,0.4,0.6,0.8],min_len=0.2, name = 'dw', sub_dir = 'MINF2')
+#plot_conf_matrix(input_dir,'dev',thresholds=[0.2,0.4,0.6,0.8],min_len=0.2, name = 'res_dw18_dev')
 
 
