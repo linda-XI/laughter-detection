d2963c15b5fe139fa5e214f992266633a0c799ff
Yuxi Dai <s1974435@uhtred.inf.ed.ac.uk>
2023-11-04 17:25:19 +0000

    modify the dw laugh not laugh plot

--- plot_result.py
+++ plot_result.py
@@ -9,5 +9,5 @@ from visualise import plot_conf_matrix_laughVSnotlaugh
 
 #plot_conf_matrix(input_dir,'dev',thresholds=[0.2,0.4,0.6,0.8],min_len=0.2, name = 'res_dw18_dev')
 input_dir = 'eval/320/dw'
-plot_conf_matrix_laughVSnotlaugh(input_dir,'test', name = 'dw_laughVSnotlaugh', sub_dir = 'MINF2')
+plot_conf_matrix_laughVSnotlaugh(input_dir,'test', name = 'dw_laughVSnotlaugh', sub_dir = 'MINF2/laughVSnotlaugh')
 
