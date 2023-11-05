ac3ea003d4743575019c924bc8eb2564e0b6cce5
Yuxi Dai <s1974435@uhtred.inf.ed.ac.uk>
2023-11-05 16:34:15 +0000

    add t0.8l0.2Bmr013chan2 textgrid to sample

--- getParsedf.py
+++ getParsedf.py
@@ -1,4 +1,6 @@
 from analysis.transcript_parsing import parse
 import pandas as pd
+from analysis import preprocess as prep
 
-parse.laugh_only_df.to_csv('./laugh_only_df.csv', index=False)
+#parse.laugh_only_df.to_csv('./laugh_only_df.csv', index=False)
+print(prep.laugh_index)
