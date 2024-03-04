b90fe308ba92f7e968c50402cfbce756a8ab9766
linda-XI <55888555+linda-XI@users.noreply.github.com>
2024-03-04 23:44:42 +0000

    0800

--- config.py
+++ config.py
@@ -256,14 +256,14 @@ ANALYSIS= {
     # "extra_laugh_sample": str(root_path / 'sample/extra_laugh_sample'),
     "extra_laugh_sample": str(root_path / 'seedModel/extraLaughSample'),
     # adding extra laugh into laugh_only_df with given threshold and minLen
-    "threshold": 0.6,
+    "threshold": 0.8,
     "minLen": 0.0,
     # dataframe are loaded from disk if possible. This option forces re-computation 
     # If True analyse.py will take a lot longer
     "force_df_recompute": False,
-    "cache_file" : ".cache/preprocessed_indices_0600.pkl",
-    "output_dir" : "output/0600_output",
-    "data_dfs_dir" : "./data/icsi/0600_data_dfs"
+    "cache_file" : ".cache/preprocessed_indices_0800.pkl",
+    "output_dir" : "output/0800_output",
+    "data_dfs_dir" : "./data/icsi/0800_data_dfs"
 }
 
 ANALYSIS['model'] = {
