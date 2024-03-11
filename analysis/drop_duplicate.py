4af2a80ab44e4bc5d88d13a40bc34f70ff83332d
linda-XI <55888555+linda-XI@users.noreply.github.com>
2024-03-11 20:49:21 +0000

    fix bug

--- analysis/drop_duplicate.py
+++ analysis/drop_duplicate.py
@@ -3,6 +3,8 @@ import os
 
 file_path = 'sample/extraLaughSample/extraLaughSample/'
 new_path = 'sample/extraLaughSample/noduplicate/'
+if not os.path.isdir(new_path):
+    os.mkdir(new_path)
 for filename in os.listdir(file_path):
     full_path = os.path.join(file_path, filename)
     df = pd.read_csv(full_path)
